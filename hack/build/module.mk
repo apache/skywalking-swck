210c552f4c50c72c770c4ace39d14a3432071892
kezhenxu94 <kezhenxu94@apache.org>
2025-11-06 11:33:51 +0800

    chore(deps): bump up go version

--- hack/build/module.mk
+++ hack/build/module.mk
@@ -23,7 +23,7 @@ tool_bin := $(root_dir)/bin
 
 include $(root_dir)/hack/build/base.mk
 
-CONTROLLER_GEN_VERSION := v0.17.0
+CONTROLLER_GEN_VERSION := v0.19.0
 KUSTOMIZE_VERSION := v4.5.6
 GOLANGCI_LINT_VERSION := v1.63.4
 
